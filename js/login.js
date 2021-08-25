7d62b6b9bfa23bc8a013f1189d14a487425636d7
gamboa15791 <88436975+gamboa15791@users.noreply.github.com>
2021-08-25 15:04:27 -0300

    carga de loging entrega 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,39 @@
+/* Se usa la función verificar para comprobar que el ingreso de la información no esté vacío*/
+
+function verificar() {
+
+    /* Creamos la variable dato que será igual a lo que ingresemos en el campo con id user*/
+    let dato = document.getElementById('user');
+
+    /*creamos un array vacio para incluir en orden la info asociada a dato */
+    let usuario = {};
+
+    /*realizo la condición de la verificación, extrayendo el valor de la variable dato
+    y quitando los espacios antes y despues con ".trim()" */
+    if (dato.value.trim() === '') {
+
+        alert('Está vacío');
+    }
+    else {
+        location.href = 'index.html';
+        usuario.nombre = dato.value; // guarda en 'usuario' el elemento 'nombre' con valor 'lo escrito en dato'
+        usuario.estado = 'conectado'; //estado del usuario
+        localStorage.setItem('usuario', JSON.stringify(usuario));
+    }
+}
+
+function desconectar(){
+        localStorage.clear();
+
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+  
+  
 });
